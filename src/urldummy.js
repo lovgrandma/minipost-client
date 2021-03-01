1b7ba55e54f4babefc90e088c1ead5df560e0f4a
Jesse <jessealexanderthompson@gmail.com>
2021-03-01 10:45:50 -0500

    update comment to url dummy

--- src/urldummy.js
+++ src/urldummy.js
@@ -3,7 +3,7 @@
 These are local variables that determine how the front end interacts with the backend or where the app is located in the network */
 
 const devurl = 'http://localhost:' + (process.env.PORT || 3000) + "/"; // 3000 for local. 3001 for docker container
-const productionurl = 'https://www.minipost.app/';
+const productionurl = 'https://www.minipost.app/'; // This is the production url. This can just be for reference
 const currentrooturl =  devurl;
 
 export default currentrooturl;
