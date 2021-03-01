767f6f5879dae3ab2e09eec6b6426764f3408a9f
Jesse <jessealexanderthompson@gmail.com>
2021-03-01 09:50:57 -0500

    removed url file from git

--- src/url.js
+++
@@ -1,5 +0,0 @@
-const devurl = 'http://localhost:' + (process.env.PORT || 3000) + "/"; // 3000 for local. 3001 for docker container
-const productionurl = 'https://www.minipost.tv/';
-const currentrooturl =  devurl;
-
-export default currentrooturl;
