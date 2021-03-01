7c0df8564b79f957266a23f00329291977c355e6
Jesse <jessealexanderthompson@gmail.com>
2021-03-01 09:51:34 -0500

    Update urldummy.js

--- src/urldummy.js
+++ src/urldummy.js
@@ -1,4 +1,6 @@
-/* These are local variables that determine how the front end interacts with the backend or where the app is located in the network */
+/* Create a url.js file in this directory with this format
+
+These are local variables that determine how the front end interacts with the backend or where the app is located in the network */
 
 const devurl = 'http://localhost:' + (process.env.PORT || 3000) + "/"; // 3000 for local. 3001 for docker container
 const productionurl = 'https://www.minipost.app/';
