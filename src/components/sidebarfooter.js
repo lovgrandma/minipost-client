76bcdb0d31422b44f6d39035346cc77b243f1d9e
Jesse <jessealexanderthompson@gmail.com>
2021-09-17 13:38:13 -0400

    privacy page

--- src/components/sidebarfooter.js
+++ src/components/sidebarfooter.js
@@ -24,6 +24,7 @@ export default class sidebarfooter extends Component {
                 <div className={this.props.username ? "sidebarfooter-loggedin" : "sidebarfooter"}>
                     <div className="sidebarfooter-list">
                         <div className="footeritem sidebarfooter"><Link to={{ pathname: `about`}}>About</Link></div>
+                        <div className="footeritem sidebar-footer"><Link to={{ pathname: `privacy` }}>Privacy</Link></div>
                         <div className="footeritem sidebar-footer"><Link to={{ pathname: `vendorapplication` }}>Vendor</Link></div>
                         <div className="footeritem sidebar-footer"><Link to={{ pathname: `letter` }}>Letter</Link></div>
                         {logoutDiv}
